062e8eb0c391bba9e07e1c65d325dc4240a06ea0
cataclysm78 <cataclysm78@yandex.ru>
2021-07-16 11:58:35 +0300

    фиксация ошибки bluetooth_adapter_winrt.cc

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
 
 url = "https://www.worldometers.info/world-population/population-by-country/"
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome(
+    options=options, executable_path=r'C:\Users\serge\Documents\projects\python\browserAutomation\chromedriver.exe')
 
 browser.get(url)
+
+# assert "Population by Country" in browser.title
+
+elem_a = browser.find_elements_by_tag_name("input")
+print(elem_a)
