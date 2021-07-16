27bbb4f44a167d8f9ccf1d4bbcc5e89b617fd332
cataclysm78 <cataclysm78@yandex.ru>
2021-07-16 13:10:55 +0300

    добавляем список стран в массив elem_text

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ browser = webdriver.Chrome(
 
 browser.get(url)
 
-# assert "Population by Country" in browser.title
-
-elem_a = browser.find_element_by_css_selector('input[type="search"]')
-print(elem_a)
-elem_a.send_keys("Russia\n")
+elem_a = browser.find_elements_by_xpath('//*[@id="example2"]/tbody/tr/td/a')
+elem_text = []
+for elem in elem_a:
+    print(str(elem.text))
+    elem_text.append(elem.text)
+print(elem_text)
+# //*[@id="example2"]/tbody/tr[2]/td[2]/a
+# print(elem_a)
+# elem_a.send_keys("Russia\n")
